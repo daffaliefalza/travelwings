fcb1055259291c4ddc0cb446d30fb27b5db7b8dc
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2024-11-11 14:16:55 +0700

    add: css for hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import "../styles/hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="container">
+        <h1>
+          Mau Liburan? <br /> Di <span className="colored">T</span>ravelWings
+          Aja!
+        </h1>
+        <button>Explore</button>
+      </div>
+    </section>
+  );
+};
+export default Hero;
